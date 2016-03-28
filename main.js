52b49ad533112751f7a0d44fadfed2186881da09
onyzhin <o.nyzhin@gmail.com>
2016-03-28 18:22:55 +0300

    add sortFunc and sortBubble

--- main.js
+++ main.js
@@ -5,18 +5,39 @@
   app.controller('sortCtrl', ['$scope', '$filter', function ($scope, $filter) {
 
     $scope.list = [
-      ["akm", 90, "hey", "bom", 98, 01, 5, "ef,", "bam"],
-      [2, 5, 94, 7, 18, 3]
+      ["93", 90, "hey", "bom", 98, 01, 5, "ef", "bam"],
+      [12, 5, 94, 7, 2, 18, 3]
     ];
 
 
     $scope.reverse =  true;
 
-    $scope.sort = function(listToSort){
+    $scope.sortFilter = function(listToSort){
       orderBy = $filter('orderBy');
       $scope.list[listToSort] = orderBy($scope.list[listToSort], '', $scope.reverse);
       console.log($scope.list[listToSort]);
       $scope.reverse = !$scope.reverse;
+    }
+
+    $scope.sortFunc = function(listToSort){
+      $scope.list[listToSort].sort(function(a,b){
+        return a-b;
+      }); 
+    }
+
+    //gри каждом проходе алгоритма по внутреннему циклу, наибольший элемент массива ставится на своё место в конце массива
+    $scope.sortBubble  = function(listToSort){
+      var tempArray = $scope.list[listToSort];
+      var length = tempArray.length-1;
+      for (var i=0; i<length; i++){
+        for (var j=0; j<length; j++){
+          if (tempArray[j]>tempArray[j+1]){
+            var temp = tempArray[j];
+            tempArray[j] = tempArray[j+1];
+            tempArray[j+1] = temp;
+          }
+        }
+      }
     }
 
   }]); 
